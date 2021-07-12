32ba7872ac01286d396084f49b9e9d30ddd77031
JIMIN <qkdwlals613@naver.com>
2021-07-12 15:43:57 +0900

    (N) [1주차] 아스키 코드 (21.07.12)
    아스키 코드 변환

---
+++ AlgorithmStudy/src/week1/Problem1.java
@@ -0,0 +1,20 @@
+package week1;
+
+import java.util.*;
+
+/**
+ * 백준 11654번
+ * 아스키 코드
+ *
+ */
+public class Problem1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String s = input.next();
+		int i = s.charAt(0);
+		
+		System.out.println(i);
+	}
+
+}

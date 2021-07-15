631ca770a7b8a5d8015a1c4ea306c87cf89ad2ad
JIMIN <qkdwlals613@naver.com>
2021-07-16 03:24:57 +0900

    (H) [1주차] 부녀회장이 될테야 (21.07.15)
    수정

--- AlgorithmStudy/src/week1/Problem4.java
+++ AlgorithmStudy/src/week1/Problem4.java
@@ -21,9 +21,9 @@ public class Problem4 {
         }
 
         // 1층 ~ 14층 채우기
-        for(int i=1 ; i<apart[0].length ; i++) {
+        for(int i=1 ; i<apart.length ; i++) {
             apart[i][0] = 0;
-            for(int j=1 ; j<apart.length ; j++) {
+            for(int j=1 ; j<apart[i].length ; j++) {
                 apart[i][j] = apart[i][j-1] + apart[i-1][j]; // k층 n호 인원 = (k층 n-1호 + k-1층 n호) 인원 
             }
         }

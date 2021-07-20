a010e6edc997c9a5ad692ada753abe6185957698
REEENG9 <qkdwlals613@gmail.com>
2021-07-20 11:13:42 +0900

    (H) [2주차] 이진수 변환 (21.07.20)

--- AlgorithmStudy/src/week2/Problem9.java
+++ AlgorithmStudy/src/week2/Problem9.java
@@ -8,22 +8,27 @@ import java.util.*;
  * 분류 : 수학, 구현, 재귀
  */
 public class Problem9 {
+    static StringBuilder sb = new StringBuilder(); // 이진수 저장
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
+        long N = input.nextLong(); // 테스트 케이스의 최대값이 100,000,000,000,000 이므로 long 타입 선언
 
         BinaryTrans(N);
+        System.out.println(sb.reverse().toString()); // StringBuilder에 거꾸로 저장되므로 reverse
 
 		input.close();
 	}
-
-    public static int BinaryTrans(int n) {
-        if(n == 1) {
-            System.out.print(1);
+    /**
+     * 재귀를 통해 주어진 정수 n을 이진수로 변환하여 StringBuilder에 저장한다
+     * @param n
+     */
+    public static int BinaryTrans(long n) {
+        if(n == 1) { // 종료조건
+            sb.append(1);
             return 1;
         }
         else {
-            System.out.print(n%2);
+            sb.append(n%2);
             return BinaryTrans(n/2);
         }
     }
